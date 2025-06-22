b0f045b5202c44907077b1f5ed2095afe7496dcd
ShellAce1 <Quentabien1@gmail.com>
2025-06-21 21:26:59 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CalendarGrid from "./CalendarGrid.jsx";
+import CalendarGrid from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<CalendarGrid />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CalendarGrid />
+  </React.StrictMode>
+);
