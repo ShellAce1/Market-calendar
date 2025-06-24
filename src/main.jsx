acd940291dce33d473c43ed7b268a9d9b6228218
ShellAce1 <Quentabien1@gmail.com>
2025-06-24 00:12:20 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CalendarGrid from "./App.jsx";
+import CalendarGrid from './CalendarGrid.jsx';
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
