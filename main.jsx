5d1b0d43e6ef9b39cb66ebcfde134d37a401c07e
ShellAce1 <Quentabien1@gmail.com>
2025-06-28 02:20:59 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CalendarGrid from "./components/CalendarGrid.jsx";
+import "./index.css"; // if you have Tailwind or your own styles
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CalendarGrid />
+  </React.StrictMode>
+);
 // React entry point
